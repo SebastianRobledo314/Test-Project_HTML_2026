be09ae46a9d94911de4a9e8a2220e12f6d79683e
SebastianRobledo314 <Scr2783@utexas.edu>
2026-01-26 12:15:28 -0600

    Layout_DraftV002

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(400, 400);
-
+  // Custom cursor setup (unchanged)
   let img = new Image();
   img.onload = function() {
     let canvas = document.createElement('canvas');
@@ -12,54 +12,38 @@ function setup() {
     document.body.style.cursor = `url('${cursorUrl}') 16 16, auto`;
   };
   img.src = 'images/Paw.png';
+
+  // Interactive image logic for multiple images
+  function setupInteractiveSwap(imgId) {
+    const imgElem = document.getElementById(imgId);
+    if (imgElem) {
+      imgElem.addEventListener('mousedown', () => {
+        imgElem.src = 'images/IMG_6809.jpeg';
+        imgElem.alt = '6809';
+      });
+      imgElem.addEventListener('mouseup', () => {
+        imgElem.src = 'images/IMG_3724.jpeg';
+        imgElem.alt = '3724';
+      });
+      imgElem.addEventListener('mouseleave', () => {
+        imgElem.src = 'images/IMG_3724.jpeg';
+        imgElem.alt = '3724';
+      });
+      imgElem.addEventListener('touchstart', () => {
+        imgElem.src = 'images/IMG_6809.jpeg';
+        imgElem.alt = '6809';
+      });
+      imgElem.addEventListener('touchend', () => {
+        imgElem.src = 'images/IMG_3724.jpeg';
+        imgElem.alt = '3724';
+      });
+    }
+  }
+
+  setupInteractiveSwap('interactive-img');
+  setupInteractiveSwap('center-img'); // If you add a center image with id="center-img"
 }
 
-function draw() {
-  background(31, 112, 200);
-  drawCat(200, 150);
-}
 
 
-function drawCat(x, y) {
-  //Body
-  fill(100, 50, 20);  // color brown
-  rect(x - 40, y + 40, 80, 60);
-  
-  // Head
-  circle(x, y, 50);
-  
-  // Left ear
-  fill(100, 50, 20);
-  triangle(x - 20, y - 25, x - 15, y - 50, x - 10, y - 25);
-  
-  // Right ear
-  triangle(x + 20, y - 25, x + 15, y - 50, x + 10, y - 25);
-  
-  // Left eye
-  noStroke();
-  fill(255);
-  circle(x - 12, y - 5, 8);
-  fill(0);
-  circle(x - 12, y - 5, 4);
-  
-  // Right eye
-  fill(255);
-  circle(x + 12, y - 5, 8);
-  fill(0);
-  circle(x + 12, y - 5, 4);
-  
-  // Nose
-  fill(255, 100, 150);
-  triangle(x - 3, y + 5, x + 3, y + 5, x, y + 10);
-  
-  // Mouth
-  stroke(0);
-  strokeWeight(2);
-  line(x, y + 10, x - 5, y + 15);
-  line(x, y + 10, x + 5, y + 15);
-  
-  // Tail
-  stroke(100, 50, 20);
-  strokeWeight(8);
-  curve(x + 40, y + 60, x + 50, y + 40, x + 60, y - 20, x + 50, y - 40);
-}
+
