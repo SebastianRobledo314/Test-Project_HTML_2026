b852de356d62c6531767db8ad45f9aecdb9c086a
SebastianRobledo314 <Scr2783@utexas.edu>
2026-01-26 12:54:49 -0600

    Layout_v005
    With Meter bar animation

--- sketch.js
+++ sketch.js
@@ -36,12 +36,41 @@ function setup() {
       imgElem.addEventListener('touchend', () => {
         imgElem.src = 'images/IMG_3724.jpeg';
         imgElem.alt = '3724';
+        clickCount++;
+        if (clickCount > maxClicks) clickCount = 0;
+        updateFill(clickCount);
+      });
+
+      imgElem.addEventListener('click', () => {
+        clickCount++;
+        if (clickCount > maxClicks) clickCount = 0;
+        updateFill(clickCount);
       });
     }
   }
 
   setupInteractiveSwap('interactive-img');
   setupInteractiveSwap('center-img'); 
+}
+
+// Fill control 
+let clickCount = 0;
+const maxClicks = 10;
+
+function updateFill(count) {
+  const fillEl = document.querySelector('.meter-fill');
+  if (!fillEl) return;
+  const fraction = count / maxClicks;
+  const heightPct = Math.round(fraction * 100);
+  fillEl.style.height = heightPct + '%';
+
+  // interpolate color from green (0,200,0) to red (255,0,0)
+  const start = { r: 0, g: 180, b: 0 };
+  const end = { r: 255, g: 0, b: 0 };
+  const r = Math.round(start.r + (end.r - start.r) * fraction);
+  const g = Math.round(start.g + (end.g - start.g) * fraction);
+  const b = Math.round(start.b + (end.b - start.b) * fraction);
+  fillEl.style.background = `rgb(${r}, ${g}, ${b})`;
 }
 
 
